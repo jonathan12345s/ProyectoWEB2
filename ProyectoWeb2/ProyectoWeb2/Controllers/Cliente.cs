c7c79314c85bb371e68eb0a7af928c201e9f0b71
DESKTOP-0K97VU7\JONATHAN <jjgarciaa@est.utn.cr>
2017-07-09 20:48:39 -0600

    se creo el pojo de clientes

---
+++ ProyectoWeb2/ProyectoWeb2/Controllers/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoWeb2.Controllers
+{
+    public class Cliente
+    {
+
+
+
+
+
+        public int id { get; set; }
+
+        public String nombre { get; set; }
+        public string cedulaJuridica { get; set; }
+
+        public String paginaWeb { get; set; }
+
+        public int numeroTelefono { get; set; }
+
+        public String Sector { get; set; }
+
+    }
+}
